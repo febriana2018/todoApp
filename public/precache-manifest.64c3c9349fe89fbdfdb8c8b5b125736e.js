b27837eecce57bceb3bbe0cfababc3111b356346
Febriana <febriana2018@mail.ugm.ac.id>
2020-05-13 10:42:04 +0700

    deploy firebase done

---
+++ public/precache-manifest.64c3c9349fe89fbdfdb8c8b5b125736e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd0cb0adb4ebb152d746579a3a009ca",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "cd5a1b39871a6206c75d",
+    "url": "/todoApp/static/css/main.f970b54f.chunk.css"
+  },
+  {
+    "revision": "f62871bebd0eb1d771b6",
+    "url": "/todoApp/static/js/2.627ad8c5.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/todoApp/static/js/2.627ad8c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd5a1b39871a6206c75d",
+    "url": "/todoApp/static/js/main.30b2e308.chunk.js"
+  },
+  {
+    "revision": "a4ca448fe860440ed276",
+    "url": "/todoApp/static/js/runtime-main.d6459f92.js"
+  }
+]);
